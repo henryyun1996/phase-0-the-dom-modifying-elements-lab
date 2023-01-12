95862d9ac3c72dffa10f46e0b8f30cc38e50274e
Henry <henrysungyun@gmail.com>
2023-01-12 01:06:37 -0800

    commit tree 92ca2994688017757231ff55a990b15dee906929

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const main = document.getElementsByTagName("main")[0];
+const firstMain = main.querySelector("main");
+main.remove("firstMain");
+
+const element = document.createElement("h1");
+document.body.append(element);
+const newHeader = document.getElementsByTagName("h1");
+
+element.id = "victory";
+
+element.innerHTML = "Henry is the Champion!";
