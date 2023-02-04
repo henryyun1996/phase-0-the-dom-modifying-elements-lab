6f370f14e9325eac758901b272f0da643f30d51d
Henry <henrysungyun@gmail.com>
2023-02-04 15:28:14 -0800

    this looks correct on my end

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+let newHeader = document.createElement("h1");
+document.body.append(newHeader);
+newHeader.id = "victory"
+
+newHeader.innerHTML = "Henry is the champion"
